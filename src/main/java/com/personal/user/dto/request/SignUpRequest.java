1f360a328dcc63aca6301bb014030f06705d6957
seongeun308 <kse011010@gmail.com>
2025-02-17 16:02:52 +0900

    KAN-8 (feat): 요청데이터 유효성 검사
    - validation을 활용해 요청데이터 유효성 검사 수행
    - 비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 포함
    - 이메일은 이메일 형식에 맞게 검사 수행

---
+++ src/main/java/com/personal/user/dto/request/SignUpRequest.java
@@ -0,0 +1,20 @@
+package com.personal.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class SignUpRequest {
+    @Email(message = "올바르지 않은 이메일 형식입니다.")
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    private String email;
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 포함해야 합니다.")
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    private String password;
+}
