4fe6ad231177a042c5c6595e8cd692077ddcbfa8
seongeun308 <kse011010@gmail.com>
2025-02-18 18:43:34 +0900

    KAN-12 (test): 이메일 중복 테스트

--- src/test/java/com/personal/user/service/UserAccountServiceImplTest.java
+++ src/test/java/com/personal/user/service/UserAccountServiceImplTest.java
@@ -1,11 +1,17 @@
 package com.personal.user.service;
 
+import com.personal.user.application.common.api.StatusCode;
+import com.personal.user.application.common.exception.user.UserAccountException;
 import com.personal.user.core.service.UserAccountService;
 import com.personal.user.application.dto.request.SignUpRequest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -18,5 +24,16 @@ class UserAccountServiceImplTest {
     void signUp() {
         SignUpRequest signUpRequest = new SignUpRequest("test@test.com", "test");
         userAccountService.signUp(signUpRequest);
+    }
+
+    @Test
+    void duplicateEmail() {
+        SignUpRequest signUpRequest = new SignUpRequest("test@test.com", "test");
+        userAccountService.signUp(signUpRequest);
+
+        UserAccountException e = assertThrows(UserAccountException.class, () ->
+                userAccountService.duplicateEmail("test@test.com"));
+
+        assertThat(e.getStatusCode()).isEqualTo(StatusCode.EMAIL_CONFLICT);
     }
 }
