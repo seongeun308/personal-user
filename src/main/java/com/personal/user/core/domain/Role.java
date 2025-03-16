f1d931ba0fea98010c44af607035352f8f2c8e3d
seongeun308 <kse011010@gmail.com>
2025-03-16 17:02:45 +0900

    KAN-26 (feat): User Role 속성 추가
    - role 속성  Role.USER로 기본값 설정

---
+++ src/main/java/com/personal/user/core/domain/Role.java
@@ -0,0 +1,6 @@
+package com.personal.user.core.domain;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/personal/user/core/domain/User.java
+++ src/main/java/com/personal/user/core/domain/User.java
@@ -2,6 +2,7 @@ package com.personal.user.core.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.redis.core.index.Indexed;
 
 @Entity
@@ -15,7 +16,11 @@ public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
     @Indexed
+    @Column(unique = true, nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
 }
-
